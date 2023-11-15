e3d003a8a33cd4ba0fd32ae68fa5690cacd8ed0f
azril <azriljanuar661@gmail.com>
2023-11-15 11:01:18 +0700

    master

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,6 @@
 const sidebar = document.getElementById("sidebar");
 const minimizeButton = document.getElementById("minimize");
 
-// Fungsi untuk memeriksa ukuran layar dan mengubah kelas "sidebar" sesuai
 function checkScreenWidth() {
   if (window.innerWidth <= 1024) {
     sidebar.classList.add("small");
@@ -10,14 +9,11 @@ function checkScreenWidth() {
   }
 }
 
-// Panggil fungsi saat halaman dimuat dan saat ukuran layar berubah
 window.addEventListener("load", checkScreenWidth);
 window.addEventListener("resize", checkScreenWidth);
 
-// Tambahkan event listener untuk mengklik tombol minimize
 minimizeButton.addEventListener("click", () => {
   sidebar.classList.toggle("small");
 });
 
-// Panggil fungsi untuk memeriksa ukuran layar saat halaman dimuat
 checkScreenWidth();
